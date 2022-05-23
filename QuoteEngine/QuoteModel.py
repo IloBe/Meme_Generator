646ab50df1e1b983df939f4aec7255b4c9d1c237
Ilona Brinkmeier <39468450+IloBe@users.noreply.github.com>
2022-05-23 21:31:55 +0200

    Feat: Check code and doc style

--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,20 +1,22 @@
-""" Encapsulates body and author class attributes. """
+"""Encapsulates body and author class attributes."""
+
 
 ##############################
 # Coding
 ##############################
-
 class QuoteModel:
-    """ Class delivers the quote text body and its author """
+    """Class delivers the quote text body and its author."""
+
     def __init__(self, body="", author=""):
-        """ Initialise the instance object. """
+        """Initialise the instance object."""
         self.body = body
         self.author = author
 
     def __str__(self):
-        """ Return a instance explanation. """
+        """Return a instance explanation."""
         return f'Text quote body is:\n{self.body},\nby author {self.author}'
 
     def __repr__(self):
-        """ Return the technical instance information. """
+        """Return the technical instance information."""
         return f'"{self.body}" - {self.author}'
+
