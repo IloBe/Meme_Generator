722352ab27c6c2ca94317acfedbd4a2151dbd3a1
Ilona Brinkmeier <39468450+IloBe@users.noreply.github.com>
2022-05-23 21:14:12 +0200

    Feat: Check code and doc style

--- MemeGenerator/JPGImageIngestor.py
+++ MemeGenerator/JPGImageIngestor.py
@@ -1,4 +1,4 @@
-""" ImageIngestor class of jpg/jpeg image files """
+"""ImageIngestor class of jpg/jpeg image files."""
 
 ##############################
 # Imports
@@ -14,18 +14,21 @@ from .ImageIngestorInterface import ImageIngestorInterface
 ##############################
 class JPGImageIngestor(ImageIngestorInterface):
     """
-    Concrete child class of the ImageIngestorInterface
+    Concrete child class of the ImageIngestorInterface.
+
+    This class handles jpg images.
     """
 
-    allowed_extensions = ['jpg', 'jpeg', 'JPEG', 'JPG']
+    allowed_extensions = ['jpg', 'jpeg', 'JPEG', 'JPG', 'webp', 'WEBP']
 
     def __str__(self):
+        """Return JPGImageIngestor class information."""
         return 'JPGImageIngestor child class of ImageIngestorInterface.'
 
     @classmethod
     def modify_image(cls, img_path, width=500) -> str:
         """
-        Processing of image; being a jpg image scaling is done.
+        Preprocessing of image; being a jpg image, scaling is done.
 
         input:
         img_path: (str) path of image used for preprocessing
@@ -33,22 +36,17 @@ class JPGImageIngestor(ImageIngestorInterface):
 
         return: path string of preprocessed image
         """
-        img_ext = pathlib.Path(img_path).suffix
+        img_ext = pathlib.Path(img_path).suffix  # image extension
+        print(f'===> JPG ingestor: img ext: {img_ext}')
         if not cls.can_ingest(img_ext):
             cls.LOGGER.error(f'Wrong image type to create meme: {img_path}')
             raise Exception(f'JPGImageIngestor cannot ingest image {img_path}')
 
         save_path = cls.PROJECT_PHOTOS
         img_parent = pathlib.Path(img_path).parent
-        # img_stem = pathlib.Path(img_path).stem  # file name without extention
-        # img_extension = pathlib.Path(img_path).suffix
         img_name = pathlib.Path(img_path).name
-        cls.LOGGER.info(f'JPGImageIngestor: img_parent: {img_parent}, img_name: {img_name}')
-
-        # for image scaling, image shall not be too small or big
-        if width < 50 or width > 500:
-            cls.LOGGER.info('Not acceptable width range of 50-500: set to 500.')
-            width = 500
+        cls.LOGGER.info(
+            f'JPG ingestor: img_parent: {img_parent}, img_name: {img_name}')
 
         try:
             with Image.open(img_path) as in_img:
@@ -59,21 +57,25 @@ class JPGImageIngestor(ImageIngestorInterface):
                     width = 500
                 else:
                     width = in_img.size[0]
-                width = width if in_img.size[0] > 500 else in_img.size[0]
-                new_height = int(width * in_img.size[1] / float(in_img.size[0]))
+                new_height = int(width*in_img.size[1] / float(in_img.size[0]))
                 new_size = (width, new_height)
-                in_img.resize(new_size)
-                #in_img.show()  # for testing only
+                in_img = in_img.resize(new_size, Image.ANTIALIAS)
+                # in_img.show() # only for testing
                 cls.LOGGER.info(f'New mod image size: {in_img.size}')
-                print(f'mod image size: {in_img.size}')
+
+                # convert webp to jpg format if necessary before saving
+                if img_ext in ['.webp', '.WEBP']:
+                    in_img = in_img.convert('RGB')
+                    img_name = img_name.lower().removesuffix('.webp') + '.jpg'
+
                 save_path = pathlib.Path.joinpath(img_parent, 'mod_'+img_name)
-                cls.LOGGER.info(f'Modified image for meme creation: {save_path}')
-				
-				# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
+                # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
                 in_img.save(save_path, format='jpeg')
         except (OSError, FileNotFoundError) as err:
-            cls.LOGGER.error(f'Error to create/save modified image: {err}')
+            cls.LOGGER.error(f'Error to create/save modified image:\n +\
+                        error message: {err}')
             print(f'JPGImageIngestor cannot create/save modified {img_path}')
+            raise
 
-        cls.LOGGER.info(f'Path of modified image for meme creation: {save_path}')
+        cls.LOGGER.info(f'Modified image path for meme creation: {save_path}')
         return str(save_path)
