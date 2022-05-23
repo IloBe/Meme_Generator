fa8254b02f2cf6a49988c7f9027d18edbaa8fa07
Ilona Brinkmeier <39468450+IloBe@users.noreply.github.com>
2022-05-23 21:12:44 +0200

    Feat: Check code and doc style

--- MemeGenerator/ImageIngestorInterface.py
+++ MemeGenerator/ImageIngestorInterface.py
@@ -1,4 +1,4 @@
-""" ImageIngestor interface for images as abstract class """
+"""ImageIngestor interface for images as abstract class."""
 
 ##############################
 # Imports
@@ -13,12 +13,15 @@ from abc import ABC, abstractmethod
 ##############################
 class ImageIngestorInterface(ABC):
     """
+    Abstract interface for image handling.
+
     ImageIngestorInterface implements the following methods:
         - can_ingest class method  -> boolean: to decide if a
           compatible image ingestor file exists
         - modify_image abstract class method: signature
           that must be realised in children classes of this interface
-        """
+    """
+
     LOGGER = logging.getLogger(__name__)
     PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
     PROJECT_PHOTOS = PROJECT_ROOT / '_data' / 'photos'
@@ -27,11 +30,11 @@ class ImageIngestorInterface(ABC):
 
     @classmethod
     def can_ingest(cls, path) -> bool:
-        """ Check the correct image type for meme creation """
+        """Check the correct image type for meme creation."""
         ext = path.split('.')[-1]
         return ext in cls.allowed_extensions
 
     @classmethod
     @abstractmethod
     def modify_image(cls, img_path: str, width: int) -> str:
-        """ Preprocessing of image """
+        """Preprocess of image."""
